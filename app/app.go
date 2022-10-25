2f33b73f31a4c2f7e88fd49d62451f0e7f7250aa
Diego Rodrigues <diegokod@gmail.com>
2022-10-24 21:09:17 -0300

    Basic files and pré-configurations

--- app/app.go
+++ app/app.go
@@ -2,6 +2,11 @@ package app
 
 import "github.com/urfave/cli"
 
+// Generate will return the CLI application ready to execute
 func Generate() *cli.App {
+	app := cli.NewApp()
+	app.Name = "CLI application"
+	app.Usage = "Search IPs and server names in the internet"
 
+	return app
 }
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"cli_server/app"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Starting main function...")
+
+	application := app.Generate()
+
+	if error := application.Run(os.Args); error != nil {
+		log.Fatal(error)
+	}
+
 }
