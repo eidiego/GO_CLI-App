e85c263da299a9db5dfe9c3588be19fa1533ee20
Diego Rodrigues <diegokod@gmail.com>
2022-10-24 21:35:56 -0300

    Feat: Search Ips function add

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate will return the CLI application ready to execute
 func Generate() *cli.App {
@@ -8,5 +14,32 @@ func Generate() *cli.App {
 	app.Name = "CLI application"
 	app.Usage = "Search IPs and server names in the internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search address IPs in the internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, error := net.LookupIP(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
